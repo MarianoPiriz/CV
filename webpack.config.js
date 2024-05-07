9a68f41c6441ce521aef37e5f9191b5d33d6588d
Mariano Piriz <mariano.piriz.acevedo@gmail.com>
2024-05-07 15:34:15 -0300

    copy plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
