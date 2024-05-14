dbcbfff79f8936a3f5e380dba8673a64a5f0dc86
Mariano Piriz <mariano.piriz.acevedo@gmail.com>
2024-05-14 18:46:31 -0300

    og tags

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, 'src'),
+    static: path.resolve(__dirname, 'dist'),
     historyApiFallback: true,
     open: true,
     compress: true,
